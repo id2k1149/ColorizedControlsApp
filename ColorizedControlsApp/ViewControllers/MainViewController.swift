6046d7ee1b448bec2ca2efa61040773fc73840d3
id2k1149 <id2k1149@gmail.com>
2022-11-06 17:41:12 -0500

    add private func setColor()

--- ColorizedControlsApp/ViewControllers/MainViewController.swift
+++ ColorizedControlsApp/ViewControllers/MainViewController.swift
@@ -10,15 +10,15 @@ import UIKit
 class MainViewController: UIViewController {
 
     // MARK: - IBOutlets
-    @IBOutlet var colorView: UIView!
+    @IBOutlet weak var colorView: UIView!
     
-    @IBOutlet var redLabel: UILabel!
-    @IBOutlet var greenLabel: UILabel!
-    @IBOutlet var blueLabel: UILabel!
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
     
-    @IBOutlet var redSlider: UISlider!
-    @IBOutlet var greenSlider: UISlider!
-    @IBOutlet var blueSlider: UISlider!
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
     
     // MARK: - LifeCicle view
     override func viewDidLoad() {
@@ -28,46 +28,30 @@ class MainViewController: UIViewController {
         colorView.layer.borderColor = UIColor.gray.cgColor
         colorView.layer.cornerRadius = colorView.frame.height / 8
         
-        colorView.backgroundColor = UIColor(
-            red: CGFloat(redSlider.value),
-            green: CGFloat(greenSlider.value),
-            blue: CGFloat(blueSlider.value),
-            alpha: 1
-        )
+        setColor()
         
         redLabel.text = String(format: "%.2f", redSlider.value)
         greenLabel.text = String(format: "%.2f", greenSlider.value)
         blueLabel.text = String(format: "%.2f", blueSlider.value)
     }
     
-    // MARK: - IB Actions
+    // MARK: - IBActions
     @IBAction func rgbSlider(_ sender: UISlider) {
+        setColor()
+        
+        redLabel.text = String(format: "%.2f", redSlider.value)
+        greenLabel.text = String(format: "%.2f", greenSlider.value)
+        blueLabel.text = String(format: "%.2f", blueSlider.value)
     }
     
-    // MARK: - Private Methods
-    private func sliderAction(label: UILabel, slider: UISlider) {
-        label.text = "\(round(slider.value * 100 / 255) / 100 )"
-        
-        switch label {
-        case redLabel:
-            colorView.backgroundColor =  UIColor(
-                red: CGFloat(slider.value) / 255,
-                green: CGFloat(greenSlider.value) / 255,
-                blue: CGFloat(blueSlider.value) / 255,
-                alpha: 1)
-        case greenLabel:
-            colorView.backgroundColor =  UIColor(
-                red: CGFloat(redSlider.value) / 255,
-                green: CGFloat(slider.value) / 255,
-                blue: CGFloat(blueSlider.value) / 255,
-                alpha: 1)
-        default:
-            colorView.backgroundColor =  UIColor(
-                red: CGFloat(redSlider.value) / 255,
-                green: CGFloat(greenSlider.value) / 255,
-                blue: CGFloat(slider.value) / 255,
-                alpha: 1)
-        }
+    // MARK: - Private methods
+    private func setColor() {
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
     }
 }
 
