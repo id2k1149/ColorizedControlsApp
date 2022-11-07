320c95c5c2403bd702cfe47b586678de8395acee
id2k1149 <id2k1149@gmail.com>
2022-11-06 21:58:32 -0500

    get colors from Start screen

--- ColorizedControlsApp/ViewControllers/StartViewController.swift
+++ ColorizedControlsApp/ViewControllers/StartViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class StartViewController: UIViewController {
 
+    @IBOutlet var screenBackgroundLabel: UIView!
+    
+    var red: CGFloat = 0
+    var green: CGFloat = 0
+    var blue: CGFloat = 0
+    var alpha: CGFloat = 0
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
+        
+        screenBackgroundLabel.backgroundColor?
+            .getRed(&red, green: &green, blue: &blue, alpha: &alpha)
+        
     }
-    
-
 }
