353305adda9291d30c03ce8724689922679d13f1
id2k1149 <id2k1149@gmail.com>
2022-11-06 19:48:51 -0500

    add StartViewController

---
+++ ColorizedControlsApp/ViewControllers/StartViewController.swift
@@ -0,0 +1,19 @@
+//
+//  StartViewController.swift
+//  ColorizedControlsApp
+//
+//  Created by Max Franz Immelmann on 11/6/22.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+}
